5f0c152983b21227eba24e6267dc1f29c1b36abd
Shahin Sadik <shahinsadik@gmail.com>
2024-05-17 09:00:23 +0600

    first commit

---
+++ src/app/config/index.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import Path from 'path';
+dotenv.config({ path: Path.join((process.cwd(), 'env')) });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
